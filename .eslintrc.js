10c598b975a9962f5903d556cd5740d2a97186ad
Geoff Lankow <geoff@darktrojan.net>
2019-05-21 15:58:52 +1200

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+/* eslint-env node */
+
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    "plugin:mozilla/recommended",
+  ],
+  parserOptions: {
+    ecmaVersion: 9,
+  },
+  globals: {
+    browser: true,
+  },
+  rules: {
+    "curly": 2,
+    "indent": [2, 2, { SwitchCase: 1, }],
+    "padded-blocks": [2, "never"],
+  },
+};
